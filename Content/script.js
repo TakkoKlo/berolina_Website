6c054e55710c9bd8cc3e18041932a6fdc7ed08c3
TakkoKlo <76124580+TakkoKlo@users.noreply.github.com>
2024-03-28 22:20:30 +0100

    Fertigstellung

--- Content/script.js
+++ Content/script.js
@@ -147,7 +147,7 @@ function deleteCookie(name) {
 }
 
 function getCookieValue(name) {
-    const regex = new RegExp(`(^| )${name}=([^;]+)`)
+    const regex = new RegExp(`(^| )${"name"}=([^;]+)`)
     const match = document.cookie.match(regex)
     if (match) {
         return match[2]
